b0874cdfb0fa87640253f13d564bf06bbe631306
aerynchakrabarty <aerynchakrabarty@gmail.com>
2025-03-19 15:37:30 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,21 @@
+ <?php
+$rand_int = rand(10, 25);
+$rand_string = "Random String";
+$rand_array = array("red", "green", "blue");
+$rand_array2 = array(3.4, 6.7, 9.2);
+echo $rand_int;
+echo "<br>";
+echo $rand_string;
+echo "<br>";
+echo $rand_array[0];
+echo "<br>";
+echo $rand_array[1];
+echo "<br>";
+echo $rand_array[2];
+echo "<br>";
+echo $rand_array2[0];
+echo "<br>";
+echo $rand_array2[1];
+echo "<br>";
+echo $rand_array2[2];
+?>
