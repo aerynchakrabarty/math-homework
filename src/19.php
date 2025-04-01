99f1e7dd868788ae66cef22d2ce02d7b751214d5
aerynchakrabarty <aerynchakrabarty@gmail.com>
2025-04-01 16:09:16 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,9 @@
+<?php
+$randInt = rand(1, 10); // Generate a random integer between 1 and 10
+
+if ($randInt % 2 == 0) {
+    echo "The number is even.";
+} else {
+    echo "The number is odd.";
+}
+?>
