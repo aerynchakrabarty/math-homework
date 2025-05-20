a8b72f7a1d36cc3456cb0b9c1920d8a448f965cc
aerynchakrabarty <aerynchakrabarty@gmail.com>
2025-05-20 11:28:20 +0000

    Add 34.php

---
+++ src/34.php
@@ -0,0 +1,10 @@
+<?php
+// Example PHP code for mathematical operations
+$num1 = 5;
+$num2 = 3;
+
+// Addition
+echo "The sum of $num1 and $num2 is: " . ($num1 + $num2);
+
+// Subtraction
+echo "The difference between $num1 and $num2 is: " . ($num1 - $num2);
